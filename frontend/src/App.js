cfeea6138b3e550603eba0add5bd9072218c9d61
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-01-21 03:49:22 -0600

    Building header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import './blipper.css';
 
 class App extends Component { 
   render() {
   return (
     <div className="App">
+      <Header />
       Hello World
     </div>
   )}
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Header = () => <div className="blipperHeader">This is my header</div>
+
+export default Header;
