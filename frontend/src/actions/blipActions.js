f7c8fe93f2704f682477067feeb1286068cf62a0
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-01-25 13:33:20 -0600

    Setting up Users index in backend

--- frontend/src/actions/blipActions.js
+++ frontend/src/actions/blipActions.js
@@ -1,7 +1,7 @@
 export const fetchBlips = () => {
   return (dispatch) => {
     dispatch({ type: 'LOADING_BLIPS' });
-    fetch('')
+    fetch('http://localhost:3000/blips')
     .then(response => response.json())
     .then(blips => dispatch({ type: 'ADD_BLIPS', blips: blips }));
   };
