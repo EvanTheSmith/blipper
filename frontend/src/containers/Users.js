431f1b64850c7f344d3e1404ec4dbc296866a4fb
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-01-31 19:35:40 -0600

    Curry'd some functions in my User container

--- frontend/src/containers/Users.js
+++ frontend/src/containers/Users.js
@@ -10,18 +10,18 @@ class Users extends Component {
   }
 
   renderUsers = () => {
-    return this.props.users.map(user => <User key={user.id} renderFollowButton={() => this.renderFollowButton(user)} user={user} />)
+    return this.props.users.map(user => <User key={user.id} renderFollowButton={this.renderFollowButton(user)} user={user} />)
   }
 
-  handleFollow = (notAlreadyFollowing, { username }) => {
-    notAlreadyFollowing ? console.log(`Now following ${username}`) : console.log(`Now unfollowing ${username}`)
+  handleFollow = (notAlreadyFollowing, this_user) => () => {
+    notAlreadyFollowing ? console.log(`Now following ${this_user.username}`) : console.log(`Now unfollowing ${this_user.username}`)
   }
 
-  renderFollowButton = (this_user) => {
+  renderFollowButton = (this_user) => () => {
     let notAlreadyFollowing = !(this_user.followers.find(follower => follower.id === this.props.current_user))
     let notCurrentUser = this_user.id !== this.props.current_user;
     if (notCurrentUser) { 
-      return <button onClick={() => this.handleFollow(notAlreadyFollowing, this_user)} >{notAlreadyFollowing ? "Follow" : "Unfollow"}</button> 
+      return <button onClick={this.handleFollow(notAlreadyFollowing, this_user)} >{notAlreadyFollowing ? "Follow" : "Unfollow"}</button> 
     }
   }
 
