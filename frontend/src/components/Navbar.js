43b67cb27201e858a17dac8bfb928eaabda186b9
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-04 16:18:57 -0600

    Added alt text to navbar banner

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
    return (
     <div className="navBar">
-        <img className="banner" src='/banner.png' />
+        <img className="banner" alt="Blipper banner" src='/banner.png' />
         <NavLink className="navLinks" to="/" exact >Home</NavLink>{" / "}
         <NavLink className="navLinks" to="/users" exact >Users</NavLink>{" / "}
         <NavLink className="navLinks" to="/users/EvanSmith" exact >Profile</NavLink>
