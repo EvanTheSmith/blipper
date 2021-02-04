2d97ba26d121997e916d054581e2cee421384093
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-04 16:37:29 -0600

    Added blips assocation to JSON output from Users backend

--- backend/app/services/user_serializer.rb
+++ backend/app/services/user_serializer.rb
@@ -6,7 +6,8 @@ class UserSerializer
   def to_serialized_json
     @user.to_json(:include => [
       {:followers => {:only => [:id, :username]}}, 
-      {:followings => {:only => [:id, :username]}}], 
+      {:followings => {:only => [:id, :username]}}, 
+      {:blips => {:only => [:id, :content, :user_id, :created_at]}}], 
     :except => [:updated_at, :created_at])
   end
     
