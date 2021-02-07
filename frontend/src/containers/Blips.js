fe28e1147c520e7cbc4e7309c9b5697035b26ca0
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-06 22:08:47 -0600

    Home page now renders blips created by current user AND created by users followed by the current user. Users are also now fetched in addition to blips upon home page load

--- frontend/src/containers/Blips.js
+++ frontend/src/containers/Blips.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { fetchBlips } from '../actions/blipActions'
+import { fetchUsers } from '../actions/userActions'
 import Blip from '../components/Blip'
 
 class Blips extends Component {
 
   componentDidMount() {
-    this.props.fetchUsers()
+    this.props.fetchUsers();
+    this.props.fetchBlips();
   }
 
   renderMyBlips = () => { 
-    let {current_user, blips} = this.props;
-    let myBlips = blips.filter(blip => blip.user.id === current_user);
-    let likedBlips = blips.filter(blip => blip.likers.some(liker => liker.id === current_user));
-    return myBlips.map(blip => <Blip key={blip.id} content={blip.content} />)
+    let {current_user, users, blips} = this.props;
+    let userObject = users.filter(user => user.id === current_user)[0];
+    if (userObject) {
+      let followedUserIDs = userObject.followings;
+      let homePageBlips = blips.filter(blip => blip.user.id === current_user || followedUserIDs.some(user => user.id === blip.user.id ));
+      return homePageBlips.map(blip => <Blip key={blip.id} content={blip.content} />)
+    }
+    return null
   }
 
   render() {
@@ -22,11 +28,14 @@ class Blips extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return { blips: state.blips, loading: state.loading, current_user: state.user_id }
+  return { users: state.users, blips: state.blips, loading: state.loading, current_user: state.user_id }
 }
 
 const mapDispatchToProps = dispatch => {
-  return { fetchUsers: () => dispatch(fetchBlips()) }
+  return { 
+    fetchBlips: () => dispatch(fetchBlips()),
+    fetchUsers: () => dispatch(fetchUsers())
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Blips)
