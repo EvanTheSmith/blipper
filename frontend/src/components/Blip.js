2b3241cff6e779fa8f44007a1792383834571c3b
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-08 04:04:26 -0600

    Shows how many likes on each blip

--- frontend/src/components/Blip.js
+++ frontend/src/components/Blip.js
@@ -2,7 +2,8 @@ import React from "react"
 
 const Blip = (props) => (
   <div className="blip">
-    "{props.blip.content}" - {props.blip.user.username}
+    "{props.blip.content}" - {props.blip.user.username} <br />
+    Likes: {props.blip.likers.length}
   </div>
 )
 
