d7c089e98699bd3a08d4ccd2a9410e8048c411a8
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-09 04:12:22 -0600

    Following and Unfollowing WORKS

--- frontend/src/reducers/blipperReducer.js
+++ frontend/src/reducers/blipperReducer.js
@@ -7,13 +7,6 @@ const blipperReducer = (state = { user_id: 1, blips: [], users: [], loading: fal
           users: [...state.users],
           loading: true
         }
-        // case 'RESOLVE_ERROR': // This sets loading back to false after an error message
-        //   return {
-        //     ...state,
-        //     blips: [...state.blips],
-        //     users: [...state.users],
-        //     loading: false
-        //   }
       case 'ADD_BLIPS':
         return {
           ...state,
@@ -25,8 +18,7 @@ const blipperReducer = (state = { user_id: 1, blips: [], users: [], loading: fal
         return {
           ...state,
           blips: [action.blip, ...state.blips],
-          users: [...state.users],
-          loading: false
+          users: [...state.users]
         }
       case 'ADD_USERS':
         return {
@@ -35,15 +27,20 @@ const blipperReducer = (state = { user_id: 1, blips: [], users: [], loading: fal
           users: action.users,
           loading: false
         }
-        case 'CHANGE_FOR_FOLLOW': // THIS IS A WORK IN PROGRESS //////////////////////////////
+        case 'CHANGE_FOR_FOLLOW': // this action updates the two users in the store involved in a follow or unfollow
           let follower = action.users[0];
           let followed = action.users[1];
+          let { users } = state; let idx; let newUsers;
+
+          idx = users.findIndex(user => user.id === follower.id)
+          newUsers = [...users.slice(0, idx), follower, ...users.slice(idx + 1)];
+          idx = users.findIndex(user => user.id === followed.id)
+          newUsers = [...newUsers.slice(0, idx), followed, ...newUsers.slice(idx + 1)];
 
           return {
             ...state,
             blips: [...state.blips],
-            users: [...state.users],
-            loading: false
+            users: newUsers
           }
        case 'CHANGE_USER': // For testing purposes, this simulates changing the current user
           return {
@@ -58,3 +55,11 @@ const blipperReducer = (state = { user_id: 1, blips: [], users: [], loading: fal
   }
    
   export default blipperReducer;
+
+// case 'RESOLVE_ERROR': // This sets loading back to false after an error message
+//   return {
+//     ...state,
+//     blips: [...state.blips],
+//     users: [...state.users],
+//     loading: false
+//   }
