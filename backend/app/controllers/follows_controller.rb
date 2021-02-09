bd8984a0b30cd52396761e9e0fca3ff85771d8e5
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-08 21:57:43 -0600

    Refactoring backend display of follow json

--- backend/app/controllers/follows_controller.rb
+++ backend/app/controllers/follows_controller.rb
@@ -3,7 +3,6 @@ class FollowsController < ApplicationController
       follower = User.find_by(id: params[:follower])
       followed_user = User.find_by(id: params[:follow])
       follow = Follow.create(follower: follower, followed_user: followed_user)
-      # render json: UserSerializer.new(users).to_serialized_json
-      render json: follow
+      render json: follow, :except => [:created_at, :updated_at]
     end
 end
