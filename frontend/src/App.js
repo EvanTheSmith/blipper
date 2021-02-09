22aa17c32b603333ad2e6fe61e39d17b4e0ba8f7
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-08 20:58:47 -0600

    User Page now loads correct page based on who is 'logged in'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import Navbar from './components/Navbar'
@@ -7,11 +8,14 @@ import UsersPage from './pages/UsersPage'
 import UserPage from './pages/UserPage'
 
 class App extends Component { 
+
+  currentUserObj = () => this.props.users.filter(user => user.id === this.props.current_user)[0];
+
   render() {
   return (
     <Router>
     <div className="App">
-      <Navbar />
+      <Navbar user={this.currentUserObj()} />
       <Route exact path="/" component={HomePage} />
       <Route exact path="/users" component={UsersPage} />
       <Route exact path="/users/:username" component={UserPage} />
@@ -20,4 +24,6 @@ class App extends Component {
   )}
 }
 
-export default App
+const mapStateToProps = (state) => { return { users: state.users, current_user: state.user_id } }
+
+export default connect(mapStateToProps)(App);
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react"
 import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = (props) => {
+   let userName = (props.user ? props.user.username : null) // sets up the user name for the User Page
    return (
     <div className="navBar">
         <img className="banner" alt="Blipper banner" src='/banner.png' />
         <NavLink className="navLinks" to="/" exact >Home</NavLink>{" / "}
         <NavLink className="navLinks" to="/users" exact >Users</NavLink>{" / "}
-        <NavLink className="navLinks" to="/users/EvanSmith" exact >Profile</NavLink>
+        <NavLink className="navLinks" to={`/users/${userName}`} exact >Profile</NavLink>
     </div>
    )
 }
 
-export default Navbar
+export default Navbar;
