d7b7a67d40f1609c6bb6c943cdb11042e83ea689
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-09 03:30:26 -0600

    Refactoring/reworking backend for creation/deletion of follows

--- backend/app/controllers/follows_controller.rb
+++ backend/app/controllers/follows_controller.rb
@@ -2,16 +2,19 @@ class FollowsController < ApplicationController
     def create
       follower = User.find_by(id: params[:follower])
       followed_user = User.find_by(id: params[:follow])
+      
       follow = Follow.create(follower: follower, followed_user: followed_user)
-      render json: follow, :except => [:created_at, :updated_at]
+
+      users = [follower, followed_user]
+      render json: UserSerializer.new(users).to_serialized_json
     end
 
     def destroy
-      follow = Follow.find{|x| x.follower_id == params[:follower] && x.followed_user_id == params[:follow]}
-      follow.destroy
-
       follower = User.find_by(id: params[:follower])
       followed_user = User.find_by(id: params[:follow])
+
+      follow = Follow.find{|x| x.follower_id == params[:follower] && x.followed_user_id == params[:follow]}
+      follow.destroy
 
       users = [follower, followed_user]
       render json: UserSerializer.new(users).to_serialized_json
