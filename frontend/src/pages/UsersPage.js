8c669b3224ebd9c3bc7e90f25074980f0f42da41
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-11 06:32:17 -0600

    Added optional ability to only show likes from other users besides self

--- frontend/src/pages/UsersPage.js
+++ frontend/src/pages/UsersPage.js
@@ -26,6 +26,7 @@ class UsersPage extends Component {
   countMyLikes = (user) => () => {
     let myBlips = this.props.blips.filter(blip => blip.user.id === user.id && blip.likers.length !== 0);
     let myLikes = myBlips.map(blip => blip.likers).flat(1);
+    // let removeOwnLikes = myLikes.filter(liker => liker.id !== user.id);
     return myLikes.length;
   }
 
