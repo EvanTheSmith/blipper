f0176034a28da1d43bb1c1f376953554a5da0279
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-17 03:45:00 -0600

    Began process to convert blip likes to image of a star

--- frontend/src/components/Blip.js
+++ frontend/src/components/Blip.js
@@ -5,6 +5,7 @@ const Blip = (props) => (
     "{props.blip.content}" - {props.blip.user.username} <br />
     Likes: {props.blip.likers.length} <br />
     {props.renderLike()}
+    <img className="blip_star" alt="blipper star" src='/star_liked.png' />
   </div>
 )
 
