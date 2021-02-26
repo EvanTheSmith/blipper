12bf806f1748c947259e7cd08d36d908d996a5bd
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-02-26 15:27:10 -0600

    converted Blip div to table

--- frontend/src/components/Blip.js
+++ frontend/src/components/Blip.js
@@ -1,14 +1,25 @@
 import React from "react"
 
 const Blip = (props) => (
-  <div className="blip">
-    "{props.blip.content}"<br />
-    {props.renderLike()}
-    Likes: {props.blip.likers.length}
-    {props.renderPoster()}
-    <br />
-    {props.renderDelete()}
-  </div>
+  <table className="blip">
+
+    <tr>
+      <td>"{props.blip.content}"</td>
+      <td>{props.renderPoster()}</td>
+    </tr>
+
+    <tr>
+      <td>
+      {props.renderLike()}
+      Likes: {props.blip.likers.length}
+      </td>
+
+      <td>
+      {props.renderDelete()}
+      </td>
+    </tr>
+
+  </table>
 )
 
 export default Blip
