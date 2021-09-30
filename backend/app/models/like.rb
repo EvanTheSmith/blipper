fbf86f4814a40af93cb380590f3df49ee26c06b2
EvanTheSmith <ProfessionalEmail@Zoho.com>
2021-09-30 16:12:29 -0500

    Added comments to like model

--- backend/app/models/like.rb
+++ backend/app/models/like.rb
@@ -1,4 +1,6 @@
 class Like < ApplicationRecord
+    # the Blip being liked
     belongs_to :liked_blip, class_name: 'Blip', foreign_key: 'blip_id'
+    # the User who liked the Blip
     belongs_to :liker, class_name: 'User', foreign_key: 'user_id'
 end
