0177da0cdf89b052a2004c33f666f371a5dba6f0
EvanTheSmith <ProfessionalEmail@Zoho.com>
2022-05-26 21:23:32 -0500

    Created a seeds.db in preparation for the database conversion

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+evan = User.create(username: "Evan");
+nancy = User.create(username: "Nancy");
+kyle = User.create(username: "Kyle");
+
+Blip.create(user_id: evan.id, content: "Change your mindset, change your life.");
+Blip.create(user_id: evan.id, content: "Practice gratitude.");
+Blip.create(user_id: evan.id, content: "It all starts with you and what you choose to think about.");
