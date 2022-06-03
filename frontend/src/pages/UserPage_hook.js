e3753a4f2b87a892b92b65abf855708a16aa0762
EvanTheSmith <ProfessionalEmail@Zoho.com>
2022-06-02 23:50:39 -0500

    slowly building the functional component version of UserPage

--- frontend/src/pages/UserPage_hook.js
+++ frontend/src/pages/UserPage_hook.js
@@ -1,53 +1,60 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
+import React from 'react'
+import { useSelector } from 'react-redux';
 import Blips from '../containers/Blips'
 import { fetchUserBlips } from '../actions/blipActions' 
 import { fetchUsers } from '../actions/userActions' 
-import { useParams } from 'react-router-dom'; // NEW //
+import { useEffect, useParams } from 'react-router-dom'; // NEW //
 
 
-class UserPage extends Component {
+function UserPage (props) {
 
-  componentDidMount() {
+  const localState = useSelector((state) => ({ 
+    users: state.users, 
+    blips: state.blips,
+    loading: state.loading, 
+    current_user: state.user_id 
+  }))
+
+  useEffect(() => { // Replaces componentDidMount
     // Fetch Users (if not already in Redux) // 
-    this.props.users.length || this.props.fetchUsers();
+    props.users.length || props.fetchUsers();
     // Fetch Blips for the Current User (if not already in Redux) //
-    if (!this.props.blips.some(blip => blip.user.id === this.props.current_user )) {
-    this.props.fetchUserBlips(this.props.current_user); }
-  }
+    if (!props.blips.some(blip => blip.user.id === props.current_user )) {
+    props.fetchUserBlips(props.current_user); }
+    }, []
+  )
 
-  countMyLikes = (user) => {
-    let myBlips = this.props.blips.filter(blip => blip.user.id === user.id && blip.likers.length !== 0);
+  const countMyLikes = (user) => {
+    let myBlips = props.blips.filter(blip => blip.user.id === user.id && blip.likers.length !== 0);
     let myLikes = myBlips.map(blip => blip.likers).flat(1);
     // OPTIONAL : filter own likes
     // let removeOwnLikes = myLikes.filter(liker => liker.id !== user.id);
     return myLikes.length;
   }
 
-  renderIfValidUser = () => {
+  const renderIfValidUser = () => {
     // let { username } = this.props.match.params; // Grab username from URL params
 
-    let canFindUser = this.props.users.filter(user => user.username === username)[0];
+    let canFindUser = props.users.filter(user => user.username === username)[0];
 
     // If the username in the URL params isn't a valid user, stop everything
     if (!canFindUser) {return "Invalid username entered into address bar"} 
 
     let user = canFindUser || {followers: []}; // prevents .username glitches below if cannot find user
-    let users_blips = this.props.blips.filter(blip => blip.user.id === user.id);
+    let users_blips = props.blips.filter(blip => blip.user.id === user.id);
 
     return (<>
     <br /><div> Welcome to {user.username}'s profile — 
       Followers: {user.followers.length} / 
       Blips: {users_blips.length} / 
-      Likes: {this.countMyLikes(user)}
+      Likes: {countMyLikes(user)}
     </div><br />
     <Blips username={username} renderMethod="User" />
     </>)
   }
 
-  render() {
-    return this.props.loading ? "Loading ..." : this.renderIfValidUser()
-  }
+
+    return props.loading ? "Loading ..." : renderIfValidUser()
 }
 
 const mapStateToProps = state => ({ 
