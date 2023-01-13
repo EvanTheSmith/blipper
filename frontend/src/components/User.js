2e50c2bc387a11c2f12f49d6dd527ea13ab64b11
EvanTheSmith <ProfessionalEmail@Zoho.com>
2023-01-13 08:22:41 -0600

    Adding comments to User.js

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -4,7 +4,11 @@ const User = (props) => (
   <div className="user_card">
     <b>{props.user.username}</b> <br />
     Followers: {props.user.followers.length} <br />
-    {props.renderFollowButton()} {/* This will only render a Follow Button if appropriate */}
+    {props.renderFollowButton()} 
+    {/* ^ This will only render a Follow Button if set conditions are met: 
+    - This user is not the active user (cannot follow self)
+    - The active user is not already following this user
+    */}
   </div>
 )
 
